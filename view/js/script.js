191ef638605be8a191d8797b17ad45a361816c1f
Miguelzzzz <148546151+Miguelzzzz@users.noreply.github.com>
2023-11-17 21:20:23 -0300

    edicao
    colocando audios nos botoes

--- view/js/script.js
+++ view/js/script.js
@@ -4,9 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     let imgPrincipal = document.querySelector('.imgPrincipal');
     let titleJogos = document.querySelector('.title-jogos');
     let descJogos = document.querySelector('.desc-jogos');
-    const selectSound = document.querySelector('#select-sound');
+    const MarioSound = document.querySelector('#mario-sound');
+    const BarbieSound = document.querySelector('#barbie-sound');
+    const OnePieceSound = document.querySelector('#one-piece-sound');
+    const WindowsXpSound = document.querySelector('#windows-xp-sound');
+    const McQueenSound = document.querySelector('#mcqueen-sound');
+    const NarutoSound = document.querySelector('#naruto-sound');
+    const BobSound = document.querySelector('#bob-sound');
+    const TrollSound = document.querySelector('#troll-sound');
+    const BattleSound = document.querySelector('#battle-sound');
+    const FoodSound = document.querySelector('#food-sound');
     let main = document.querySelector('main');
     let btnJogar = document.querySelector('.btn-jogar');
+
 
     // Button
     const buttons = document.querySelectorAll('.btn-open');
@@ -85,6 +95,8 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     }
 
+
+
     btnJogar.addEventListener('click',()=>{
         console.log('oi')
         let primeiraPagina = document.querySelector('.primeiraPagina')
@@ -92,11 +104,77 @@ document.addEventListener('DOMContentLoaded', function () {
         main.style.display='flex'
     })
 
+    function marioSound() {
+        MarioSound.currentTime = 0;
+        MarioSound.play();
+        console.log('Tentando reproduzir som do Mario');
+    }
 
-    function soundSelect(){
-        selectSound.currentTime=0
-        selectSound.play()
-        console.log('Tentando reproduzir som');
+    function barbieSound() {
+        BarbieSound.currentTime = 0;
+        BarbieSound.play();
+        console.log('Tentando reproduzir som do Mario');
+    }
+
+    function bobSound() {
+        BobSound.currentTime = 0;
+        BobSound.play();
+        console.log('Tentando reproduzir som do Mario');
+    }
+
+    function onepieceSound() {
+        OnePieceSound.currentTime = 0;
+        OnePieceSound.play();
+        console.log('Tentando reproduzir som de One Piece');
+    }
+
+    function windowsxpSound() {
+        WindowsXpSound.currentTime = 0;
+        WindowsXpSound.play();
+        console.log('Tentando reproduzir som do Windows XP');
+    }
+
+    function mcqueenSound() {
+        McQueenSound.currentTime = 0;
+        McQueenSound.play();
+        console.log('Tentando reproduzir som do Naruto');
+    }
+
+    function narutoSound() {
+        NarutoSound.currentTime = 0;
+        NarutoSound.play();
+        console.log('Tentando reproduzir som do Naruto');
+    }
+
+    function battleSound() {
+        BattleSound.currentTime = 0;
+        BattleSound.play();
+        console.log('Tentando reproduzir som da batalha');
+    }
+
+    function trollSound() {
+        TrollSound.currentTime = 0;
+        TrollSound.play();
+        console.log('Tentando reproduzir som da trolagem');
+    }
+
+    function foodSound() {
+        FoodSound.currentTime = 0;
+        FoodSound.play();
+        console.log('Tentando reproduzir som da comida');
+    }
+
+    function stopAudio() {
+        MarioSound.pause();
+        BarbieSound.pause();
+        BobSound.pause();
+        OnePieceSound.pause();
+        WindowsXpSound.pause();
+        NarutoSound.pause();
+        McQueenSound.pause();
+        BattleSound.pause();
+        TrollSound.pause();
+        FoodSound.pause();
     }
 
     buttons.forEach(button => {
@@ -104,7 +182,36 @@ document.addEventListener('DOMContentLoaded', function () {
             imgPrincipal.src = projetos[button.id]['img'];
             titleJogos.innerHTML = projetos[button.id]['title'];
             descJogos.innerHTML = projetos[button.id]['description'];
-            soundSelect();
+
+            if (button.id === 'btn-1') {
+                battleSound();
+            } else if (button.id === 'btn-2') {
+                bobSound();
+            }else if (button.id === 'btn-3') {
+                marioSound();
+            }else if (button.id === 'btn-4') {
+                onepieceSound();
+            }else if (button.id === 'btn-5') {
+                barbieSound();
+            }else if (button.id === 'btn-6') {
+                foodSound();
+            }else if (button.id === 'btn-7') {
+                windowsxpSound();
+            }else if (button.id === 'btn-8') {
+                mcqueenSound();
+            }else if (button.id === 'btn-9') {
+               narutoSound();
+            }else if (button.id === 'btn-10') {
+                trollSound();
+            }
+        });
+
+        button.addEventListener('mouseleave', () => {
+            stopAudio();
         });
     });
+
+
+
+  
 });
