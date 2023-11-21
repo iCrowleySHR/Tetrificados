728c341eafaba147bc318b3a346e26f1f1f60c08
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-11-20 21:00:48 -0300

    Começando a criação da página feedback

---
+++ model/conexao.php
@@ -0,0 +1,20 @@
+<?php 
+class conexao{
+    private $pdo;
+    public function __construct($dbname, $host, $user, $password) {
+        try {
+            $this -> pdo = New PDO ("mysql:dbname=" . $dbname . ";host=" . $host, $user, $password);
+            return "CONEXÃO DO PDO";
+        } catch (PDOException $erro) {
+            return "ERRO DE CONEXÃO NO PDO:" . $erro -> getMessage();
+            exit();
+        } catch (Exception $erro) {
+            return "ERRO NÃO PASSOU DA CONEXÃO:" . $erro -> getMessage();
+            exit();
+        }
+    }
+
+    
+}
+
+?>
