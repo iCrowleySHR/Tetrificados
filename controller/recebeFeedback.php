c397a30a8402958090bd38b3a58f93e8da583e23
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2023-11-20 21:29:21 -0300

    Criação dos arquivos php

---
+++ controller/recebeFeedback.php
@@ -0,0 +1,5 @@
+<?php
+    require_once '../model/conexao.php';
+    $config = parse_ini_file('../model/config.ini');
+    $conexao = new conexao ($config['dbname'], $config['host'], $config['user'], $config['password']);
+?>
